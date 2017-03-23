cec287ee658a9e409a3771d193f3141d10606afb
= <=>
2017-03-23 20:37:01 -0300

    Funcionandogit add .!

--- src/com/opet/Aula4Triangulo.java
+++ src/com/opet/Aula4Triangulo.java
@@ -6,7 +6,22 @@ public class Aula4Triangulo
     public static void main(String[] args)
     {
         // TODO Auto-generated method stub
-
+        int quantidadeX = 1;
+        int espacosEmBranco = 0;
+        String xis = "";
+        for(int i=1; i<=9;i++) {
+            xis = "";
+            if(i>1)
+                quantidadeX = quantidadeX+2;
+            for(int j=1;j<=quantidadeX;j++) {
+                xis = xis + "X";
+            }
+            espacosEmBranco = (19-quantidadeX)/2;
+            for(int j=1;j<=espacosEmBranco;j++) {
+                xis = " "+xis;
+            }
+            System.out.println(xis);
+        }
     }
 
 }
